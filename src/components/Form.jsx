15396175421266eacc1bf86d9f9b451697cf2a5c
Kaleab Fekade <kaleab83@gmail.com>
2023-12-05 06:34:28 -0500

    React form first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+const Form = () => {
+  return (
+    <form>
+      <label>
+        UserName 
+        <input type="text" name="username"/>
+      </label>
+      <label>
+        Email 
+        <input type="email" name="email"/>
+      </label>
+    </form>
+  )
+
+}
+
+export default Form;
